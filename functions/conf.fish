b0b87dffab5962ab296cd4ea83e0747eb7c5f601
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-08 00:50:54 +0800

    functions/conf : `XDG_CONFIG_HOME` manager
    Open or edit configuration file under `XDG_CONFIG_HOME`, by default,
    it is use `nvim` as editor and `nvim -c :Telescope find_files` as
    directory opener.
    
    The arguments is the config directory or file, if given more than one
    arguments, then the arguments will be treated as subdirectory, if the
    last arguments is a file, then it will opened using the `edit` variable,
    if a directory, the `open_editor` variable will be used.
    
    example :
    
    ```sh
    $ conf fish conf.d # will open $XDG_CONFIG_HOME/fish/conf.d/
    
    $ # will edit $XDG_CONFIG_HOME/fish/functions/conf.fish
    $ conf fish functions conf.fish
    ```
    
    On exit, it will throw back to the previous `$PWD`.

---
+++ functions/conf.fish
@@ -0,0 +1,45 @@
+function conf
+    set -l _CWD (pwd)
+
+    if test -z "$argv"
+        cd $XDG_CONFIG_HOME; or cd $HOME/.config
+        exa --only-dirs --icons
+        set --erase _CWD
+        return
+    end
+
+    set -l dir $HOME/.config
+    set -l edit e
+    set -l open_editor e -c ":Telescope find_files"
+    set -l config
+
+    for arg in $argv
+        set arg $dir/$arg
+        if test -e $arg
+            set config $arg
+            if test -d $arg
+                set dir $arg
+                cd $dir
+            end
+        else
+            printf 'No config available for %s\n' $arg
+            cd $_CWD
+            set --erase _CWD
+            return 1
+        end
+    end
+
+    if test -z $config
+        cd $_CWD
+        return 1
+    end
+
+    if test -d $config
+        $open_editor
+    else if test -f $config
+        $edit $config
+    end
+
+    cd $_CWD
+    set --erase _CWD
+end
