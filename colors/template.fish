f0401feae32f3168c01003ca3cbfc619e4460fb5
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-08 01:33:36 +0800

    colors : Add template
    Modify the file by add color definition[1] at the end of color variable
    
    example :
    
    ```
        # set -Ux variable color_definition
        set -Ux fish_color_normal blue
        set -Ux fish_color_param --bold brcyan
    ```
    
    [1]: https://fishshell.com/docs/current/cmds/set_color.html#cmd-set-color

---
+++ colors/template.fish
@@ -0,0 +1,19 @@
+set -Ux fish_color_normal # default color
+set -Ux fish_color_command # commands like echo
+set -Ux fish_color_keyword # keywords like if - this falls back on the command color if unset
+set -Ux fish_color_quote # quoted text like "abc"
+set -Ux fish_color_redirection # IO redirections like >/dev/null
+set -Ux fish_color_end # process separators like ';' and '&'
+set -Ux fish_color_error # syntax errors
+set -Ux fish_color_param # ordinary command parameters
+set -Ux fish_color_comment # comments like '# important'
+set -Ux fish_color_selection # selected text in vi visual mode
+set -Ux fish_color_operator # parameter expansion operators like '*' and '~'
+set -Ux fish_color_escape # character escapes like 'n' and 'x70'
+set -Ux fish_color_autosuggestion # autosuggestions (the proposed rest of a command)
+set -Ux fish_color_cwd # the current working directory in the default prompt
+set -Ux fish_color_user # the username in the default prompt
+set -Ux fish_color_host # the hostname in the default prompt
+set -Ux fish_color_host_remote # the hostname in the default prompt for remote sessions (like ssh)
+set -Ux fish_color_cancel # the '^C' indicator on a canceled command
+set -Ux fish_color_search_match # history search matches and selected pager items (background only)
