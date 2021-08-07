e9eaa55b0c93d9389cefc69118585613c7deb3c9
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-08 01:13:26 +0800

    colors : Add syntax higlighting files used by `setscheme`

---
+++ colors/tokyonight.fish
@@ -0,0 +1,18 @@
+set -Ux fish_color_normal blue
+set -Ux fish_color_command brgreen
+set -Ux fish_color_param --bold brcyan
+set -Ux fish_color_quote bryellow
+set -Ux fish_color_redirection --bold blue
+set -Ux fish_color_end brblue
+set -Ux fish_color_error --bold brred
+set -Ux fish_color_comment white
+set -Ux fish_color_selection brblack
+set -Ux fish_color_search_match --background=black
+set -Ux fish_color_operator --bold yellow
+set -Ux fish_color_escape cyan
+set -Ux fish_color_cwd --bold brblue
+set -Ux fish_color_autosuggestion brblack
+set -Ux fish_color_user --bold brcyan
+set -Ux fish_color_host --bold brgreen
+set -Ux fish_color_host_remote --bold red
+set -Ux fish_color_cancel red
