f74700af3899964a02bc6044e424b27169abd352
fitrh <fitrahm17h@student.unhas.ac.id>
2021-08-31 18:01:12 +0800

    functions/dotfile : Initial implementation, dotfile manager
    Use bare repository to manange my dotfiles, by default, it's use
    `~/git/dotfiles` directory as `$GIT_DIR`, so to use this function as it
    is, you must set your `$HOME` as bare repository with the following
    command:
    
        ```
        cd ~/
        git init --bare $HOME/git/dotfiles
        dotfile config --local status.showUntrackedFiles no
        dotfile remote add <remote> <url>
        ```
    
    If you want to change the `$GIT_DIR`, change the `DOTFILES` variable
    inside the `__dotfile_cmd` function and use it as `--bare` value, or
    if you already have a repository, go to that repository and run this
    command: `git config --local core.bare true`.
    
    Then, run the previous command to init your `$HOME` as bare repository.

---
+++ functions/dotfile.fish
@@ -0,0 +1,31 @@
+function dotfile
+    switch $argv[1]
+        case a
+            __dotfile_cmd add $argv[2..]
+        case stat
+            __dotfile_cmd status $argv[2..]
+        case cm
+            __dotfile_commit $argv[2..]
+        case ps
+            __dotfile_cmd push $argv[2..]
+        case '*'
+            __dotfile_cmd $argv
+    end
+end
+
+function __dotfile_cmd
+    set DOTFILES $HOME/git/dotfiles
+    set GIT (which git)
+    set CMD $GIT --git-dir=$DOTFILES --work-tree=$HOME
+    $CMD $argv
+end
+
+function __dotfile_commit
+    if test -z "$argv[1]"
+        if not _dotfile_cmd commit -S
+            return 1
+        end
+        return 0
+    end
+    _dotfile_cmd commit -S -m $argv
+end
