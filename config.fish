82d19c7458a024d37fa67ac07658596251292444
fitrh <fitrahm17h@student.unhas.ac.id>
2021-09-04 02:01:52 +0800

    config : Move `starship` related variable inside it's guard

--- config.fish
+++ config.fish
@@ -1,8 +1,7 @@
-set -l CONFIG_DIR $HOME/.config/starship
-set -l PROMPT_THEME $CONFIG_DIR/default.toml
-set -gx STARSHIP_CONFIG $PROMPT_THEME
-
 if command -sq starship
+    set -l CONFIG_DIR $HOME/.config/starship
+    set -l PROMPT_THEME $CONFIG_DIR/default.toml
+    set -gx STARSHIP_CONFIG $PROMPT_THEME
     starship init fish | source
 end
 
