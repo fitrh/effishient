1f2d67b5b2726bf37b1d63183a42b5a7fa354efc
fitrh <fitrahm17h@student.unhas.ac.id>
2021-11-09 11:20:30 +0800

    functions/c : `cd then list` wrapper, use `exa` as `ls` if exist

---
+++ functions/c.fish
@@ -0,0 +1,18 @@
+function c --wraps cd
+    set _DIR "."
+    if test -n "$argv"
+        set _DIR $argv[1]
+    end
+
+    if not test -d $_DIR
+        printf "Directory \e[1;31;40m $_DIR \e[0m doesn't exist\n"
+        return 1
+    end
+
+    cd $_DIR
+    if command -q exa
+        exa -l --icons --group-directories-first -h
+    else
+        ls --color --group-directories-first -hp
+    end
+end
