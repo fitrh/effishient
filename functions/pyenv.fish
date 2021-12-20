731f947d87171bc29678cac5c32e721849be49a2
fitrh <fitrahm17h@student.unhas.ac.id>
2021-12-20 15:45:10 +0800

    functions/pyenv: Set proper return status code

--- functions/pyenv.fish
+++ functions/pyenv.fish
@@ -58,11 +58,13 @@ function __pyenv_use_verify
         if test -d $argv/bin
             if test -f $argv/bin/activate.fish
                 . $argv/bin/activate.fish
+                return 0
             end
         end
     else
         printf "\e[1;31m%s\e[0m not a valid virtual environment.\n" $argv
     end
+    return 1
 end
 
 function __pyenv_use
