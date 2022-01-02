d2b8a73d7063119f4dbf46bb26b24982d54db2fd
fitrh <fitrahm17h@student.unhas.ac.id>
2022-01-02 22:35:13 +0800

    functions/pp: Add `$PWD` for upadate log

--- functions/pp.fish
+++ functions/pp.fish
@@ -53,7 +53,9 @@ function __pp_upgrade
     if pip list -o --format=json | jq -r .[].name >outdate.txt
         if pip install -Ur outdate.txt
             if rm -rf outdate.txt
-                pip freeze >"$CACHE/freeze_"(date +'%Y%m%d_%H%M%S')".log"
+                set -l cache_file "freeze_"(date +'%Y%m%d_%H%M%S')".log"
+                printf "# %s\n" $PWD >$CACHE/$cache_file
+                pip freeze >>$CACHE/$cache_file
             end
         end
     end
