0fbe904eb573091988a07af04ad48d22764dd541
fitrh <fitrahm17h@student.unhas.ac.id>
2022-02-11 20:16:00 +0800

    functions/notes: `cd` into directory before open the editor

--- functions/notes.fish
+++ functions/notes.fish
@@ -1,20 +1,26 @@
 function notes -d "Personal Notes manager"
+    set -l cwd $PWD
     set -l dir $HOME/documents/markdown
+    cd $dir
     if test -z "$argv"
-        glow -w 120 $dir
+        glow -w 120 .
+        cd $cwd
         return 0
     end
 
-    if set i (contains -i -- "-e" $argv)
+    if set i (contains -i -- "e" $argv)
         set -e -- argv[$i]
-        $EDITOR $dir/$argv[$i].md
+        $EDITOR $argv[$i].md
+        cd $cwd
         return 0
     end
 
-    if test -f $dir/$argv[1].md
-        glow -w 120 $dir/$argv[1].md
+    if test -f $argv[1].md
+        glow -w 120 $argv[1].md
+        cd $cwd
         return 0
     end
 
-    glow -w 120 $dir
+    glow -w 120 .
+    cd $cwd
 end
