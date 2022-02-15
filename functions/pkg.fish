aef5f131cbbf74e932848f17fea7b922baa60aff
fitrh <fitrahm17h@student.unhas.ac.id>
2022-02-15 12:04:22 +0800

    functions/pkg: Remove the last range from `argv` and add proper return

--- functions/pkg.fish
+++ functions/pkg.fish
@@ -204,33 +204,34 @@ function pkg --wraps pacman
     if test -z "$argv"
         __pkg_usage
         __pkg_zero_arguments
-        return
+        return 1
     end
 
     switch $argv[1]
         case i install
-            __pkg_install $argv[2..-1]
+            __pkg_install $argv[2..]
         case u update
-            __pkg_update $argv[2..-1]
+            __pkg_update $argv[2..]
         case d downgrade
-            __pkg_downgrade $argv[2..-1]
+            __pkg_downgrade $argv[2..]
         case s search
-            __pkg_search $argv[2..-1]
+            __pkg_search $argv[2..]
         case sy sync
             sudo pacman -Syy
         case info
-            __pkg_info $argv[2..-1]
+            __pkg_info $argv[2..]
         case c clean
-            __pkg_clean $argv[2..-1]
+            __pkg_clean $argv[2..]
         case r remove
-            __pkg_remove $argv[2..-1]
+            __pkg_remove $argv[2..]
         case q query
-            __pkg_query $argv[2..-1]
+            __pkg_query $argv[2..]
         case h help
             __pkg_usage
             __pkg_zero_arguments
         case '*'
             printf "Invalid arguments.\n"
             __pkg_zero_arguments
+            return 1
     end
 end
