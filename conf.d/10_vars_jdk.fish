092eb844a671b7498fe4c1ba557638c07b58108c
fitrh <fitrahm17h@student.unhas.ac.id>
2022-02-16 00:05:26 +0800

    conf.d/10_vars_jdk: Add `JAVA_RUNTIMES`

--- conf.d/10_vars_jdk.fish
+++ conf.d/10_vars_jdk.fish
@@ -4,6 +4,7 @@ set -l OPTIONS "-Dawt.useSystemAAFontSettings=gasp" \
     "-Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
 
 set -x JAVA_TOOL_OPTIONS $OPTIONS
-set -x JAVA_HOME $LOCAL_LIB/jvm/default
+set -x JAVA_RUNTIMES $LOCAL_LIB/jvm
+set -x JAVA_HOME $JAVA_RUNTIMES/default
 set -x JAVA_WORKSPACE $HOME/dev/0_icebox/java/workspace
 set -x JDTLS_HOME $HOME/git/eclipse.jdt.ls
