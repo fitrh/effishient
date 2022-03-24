3d6ae2d76bb390bf6b45ec8d0473ce1b4e14eff2
fitrh <fitrahm17h@student.unhas.ac.id>
2022-03-24 20:46:45 +0800

    functions/dotfile: Add subcommand for `diff` and `add --patch`
    - `d`     -> `git diff`
    - `dc`    -> `git diff --cached`
    - `patch` -> `git add --patch`
    
    This commit also rename `stat` to `s`.

--- functions/dotfile.fish
+++ functions/dotfile.fish
@@ -2,10 +2,16 @@ function dotfile --wraps git
     switch $argv[1]
         case a
             __dotfile_cmd add $argv[2..]
-        case stat
+        case d
+            __dotfile_cmd diff $argv[2..]
+        case dc
+            __dotfile_cmd diff --cached $argv[2..]
+        case s
             __dotfile_cmd status $argv[2..]
         case cm
             __dotfile_commit $argv[2..]
+        case patch
+            __dotfile_cmd add --patch $argv[2..]
         case ps
             __dotfile_cmd push $argv[2..]
         case '*'
