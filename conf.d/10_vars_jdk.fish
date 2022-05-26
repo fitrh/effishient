99bbe10e1a2a9ad5249974c566b2c536664911a5
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-05-26 17:42:18 +0800

    conf.d/10_vars_jdk: Update `JDTLS_HOME`

--- conf.d/10_vars_jdk.fish
+++ conf.d/10_vars_jdk.fish
@@ -7,4 +7,4 @@ set -x JAVA_TOOL_OPTIONS $OPTIONS
 set -x JAVA_RUNTIMES $LOCAL_LIB/jvm
 set -x JAVA_HOME $JAVA_RUNTIMES/default
 set -x JAVA_WORKSPACE $HOME/dev/0_icebox/java/workspace
-set -x JDTLS_HOME $HOME/git/eclipse.jdt.ls
+set -x JDTLS_HOME $LOCAL_LIB/jdt.ls
