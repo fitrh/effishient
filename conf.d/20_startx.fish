89c45fd964d09d3124aa8b999b8c8b855e9865fa
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-06-27 18:08:14 +0800

    conf.d/20_startx: Use tty [1-3] for X and tty 4 for wayland

--- conf.d/20_startx.fish
+++ conf.d/20_startx.fish
@@ -1,9 +1,11 @@
 # Start X at login
 if status is-login
-    if test \( -z "$DISPLAY" \) -a \( "$XDG_VTNR" -le 5 \)
+    if test \( -z "$DISPLAY" \) -a \( "$XDG_VTNR" -le 3 \)
         set -l LOG_DIR $XDG_CACHE_HOME/xorg
         set -l LOG_FILE startx_(date +'%Y%m%d_%H%M%S').log
         set -l LOG $LOG_DIR/$LOG_FILE
         exec startx -- -keeptty &>$LOG
+    else if test \( -z "$WAYLAND_DISPLAY" \) -a \( "$XDG_VTNR" -eq 4 \)
+        $XDG_CONFIG_HOME/river/start
     end
 end
