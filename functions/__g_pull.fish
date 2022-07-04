a0495b1d04b7dd5f90ff29a19d38e0cc02c9fb5a
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-07-04 21:13:24 +0800

    functions/g(pl): Show previous and current HEAD hash if updated

--- functions/__g_pull.fish
+++ functions/__g_pull.fish
@@ -1,9 +1,23 @@
 function __g_pull
+    set -l old_head (git rev-parse --short HEAD)
     if test -z "$argv"
         if not git pull --ff-only --progress --rebase=false
             return 1
         end
+        __g_pull_log $old_head
         return 0
     end
     git pull --progress $argv
+    __g_pull_log $old_head
+end
+
+function __g_pull_log
+    set -l old_head $argv[1]
+    set -l new_head (git rev-parse --short HEAD)
+    if test $old_head = $new_head
+        return 0
+    end
+    printf "\nRun "
+    printf "\e[0;92mgit log %s..%s\e[0m " $old_head $new_head
+    printf "to see the update log\n"
 end
