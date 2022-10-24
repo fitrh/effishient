b3b5ceb8c6bb719bdcce367bfc148aeddea6c2ed
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2022-10-24 13:37:31 +0800

    cond.d/20_startx: Start wayland on tty1

--- conf.d/20_startx.fish
+++ conf.d/20_startx.fish
@@ -1,11 +1,12 @@
-# Start X at login
 if status is-login
-    if test \( -z "$DISPLAY" \) -a \( "$XDG_VTNR" -le 3 \)
+    if test \( -z "$WAYLAND_DISPLAY" \) -a \( "$XDG_VTNR" -eq 1 \)
+        if test -f $XDG_CONFIG_HOME/river/start
+            exec $XDG_CONFIG_HOME/river/start
+        end
+    else if test \( -z "$DISPLAY" \) -a \( "$XDG_VTNR" -le 4 \)
         set -l LOG_DIR $XDG_CACHE_HOME/xorg
         set -l LOG_FILE startx_(date +'%Y%m%d_%H%M%S').log
         set -l LOG $LOG_DIR/$LOG_FILE
         exec startx -- -keeptty &>$LOG
-    else if test \( -z "$WAYLAND_DISPLAY" \) -a \( "$XDG_VTNR" -eq 4 \)
-        exec $XDG_CONFIG_HOME/river/start
     end
 end
