1740915a8ef3362ecd080461ef4a1b1679951df5
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-04-06 20:19:36 +0800

    functions/fish_prompt: Use single quotes whenever possible
    This change update the comments.

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,17 +2,15 @@ function fish_prompt --description 'Write out the prompt'
     set -l last_status $pipestatus
     set -l color_status $fish_color_status
 
-    # Color the prompt differently when we're root
     set -l suffix (set_color white) '%'
     if fish_is_root_user
-        set suffix (set_color red) '#'
+        set suffix (set_color red) '#' # Change the prompt suffix and color when we're root
     end
 
-    # Write pipestatus
     set -l status_color (set_color $color_status) (set_color --bold $color_status)
-    set -l code (__fish_print_pipestatus "[" "]" "|" $status_color $last_status)
+    set -l code (__fish_print_pipestatus '[' ']' '|' $status_color $last_status) # Contains exit code if it is an error
     if test -n "$code"
-        set suffix (set_color $color_status) "✗"
+        set suffix (set_color $color_status) '✗' # Change the prompt suffix on exit error
     end
 
     set dir (string shorten -m 40 -l (prompt_pwd -d 1 -D 3))
@@ -21,5 +19,5 @@ function fish_prompt --description 'Write out the prompt'
         printf '%s%s/' (set_color white) (string join '/' $dir[1..-2])
         printf '%s%s%s\n' (set_color blue --bold) $dir[-1] (set_color normal)
     end
-    echo -n -s -e $code $suffix " "
+    echo -n -s -e $code $suffix ' '
 end
