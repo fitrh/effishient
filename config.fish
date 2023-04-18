8c3302071b27c799330674a3ecb8d90935b61fdb
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-04-18 19:21:45 +0800

    config: Remove global scope from `STARSHIP_CONFIG` env
    see: https://fishshell.com/docs/current/language.html#variable-scope
    
    > When no scope is given and no variable of that name exists,
    > the variable is created in function scope if inside a function,
    > or global scope if no function is executing.

--- config.fish
+++ config.fish
@@ -5,7 +5,7 @@ end
 if command -sq starship
     set -l CONFIG_DIR $HOME/.config/starship
     set -l PROMPT_THEME $CONFIG_DIR/default.toml
-    set -gx STARSHIP_CONFIG $PROMPT_THEME
+    set -x STARSHIP_CONFIG $PROMPT_THEME
     starship init fish | source
 end
 
