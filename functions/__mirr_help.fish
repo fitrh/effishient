7da189234fd8760d5cd41799a00ca8d78a93bd01
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-05-11 21:34:48 +0800

    functions/mirr(help): Format

--- functions/__mirr_help.fish
+++ functions/__mirr_help.fish
@@ -1,5 +1,5 @@
 function __mirr_help
-    printf "\n  Usage : \e[1;32mmirr\e[0m <\e[1;33moption\e[0m> "
+    printf "\n  Usage: \e[1;32mmirr\e[0m <\e[1;33moption\e[0m> "
     printf "<\e[1;36margument\e[0m(\e[1;36ms\e[0m)>\n"
     printf "\n  Available Options \n"
     printf "    \e[1;33m-l --list\e[0m\t\t: List latest synchronized mirrors\n"
@@ -10,8 +10,8 @@ function __mirr_help
     printf "\n  Available Arguments \n"
     printf "    \e[1;36mcountry=\e[0;3;33mCOUNTRY\e[0m\t: "
     printf "Get mirrors located at \e[0;3;33mCOUNTRY\e[0m "
-    printf "(default : \e[94m%s\e[0m)\n" $__COUNTRY
+    printf "(default: \e[94m%s\e[0m)\n" $__COUNTRY
     printf "    \e[1;36minstance=\e[0;3;33mNUM\e[0m\t: "
     printf "Get top \e[0;3;33mNUM\e[0m of available mirrors "
-    printf "(default : \e[94m%s\e[0m)\n\n" $__INSTANCE
+    printf "(default: \e[94m%s\e[0m)\n\n" $__INSTANCE
 end
