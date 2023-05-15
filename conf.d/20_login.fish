40a898c261c12bd68240368c89a5dc9cd5c52482
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2023-05-15 21:13:24 +0800

    conf.d/20_login: Early return

--- conf.d/20_login.fish
+++ conf.d/20_login.fish
@@ -1,12 +1,14 @@
-if status is-login
-    if test \( -z "$WAYLAND_DISPLAY" \) -a \( "$XDG_VTNR" -eq 1 \)
-        if test -f $XDG_CONFIG_HOME/river/start
-            exec $XDG_CONFIG_HOME/river/start
-        end
-    else if test \( -z "$DISPLAY" \) -a \( "$XDG_VTNR" -le 4 \)
-        set -l LOG_DIR $XDG_CACHE_HOME/xorg
-        set -l LOG_FILE startx_(date +'%Y%m%d_%H%M%S').log
-        set -l LOG $LOG_DIR/$LOG_FILE
-        exec startx -- -keeptty &>$LOG
+if not status is-login
+    return
+end
+
+if test \( -z "$WAYLAND_DISPLAY" \) -a \( "$XDG_VTNR" -eq 1 \)
+    if test -f $XDG_CONFIG_HOME/river/start
+        exec $XDG_CONFIG_HOME/river/start
     end
+else if test \( -z "$DISPLAY" \) -a \( "$XDG_VTNR" -le 4 \)
+    set -l LOG_DIR $XDG_CACHE_HOME/xorg
+    set -l LOG_FILE startx_(date +'%Y%m%d_%H%M%S').log
+    set -l LOG $LOG_DIR/$LOG_FILE
+    exec startx -- -keeptty &>$LOG
 end
