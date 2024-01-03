7afd26306a376b7031a65999d4d33d310245be5c
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2024-01-03 19:07:44 +0800

    functions/..: Early return on `string repeat --count` error
    `string repeat --count` throws an error if `$depth` is not an integer.

--- functions/...fish
+++ functions/...fish
@@ -3,5 +3,10 @@ function .. --description 'Go to the n-th parent dir' --argument-names depth
     if test -n "$depth"
         set parent (string repeat --count $depth '../')
     end
+
+    if test $status -ne 0
+        return 1
+    end
+
     cd $parent
 end
