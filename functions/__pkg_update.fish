169040030e7013d6e156f42582e2167ea4ec0401
Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
2024-05-17 15:16:56 +0800

    functions/pkg(update): Add check for `archlinux-keyring` updates
    To prevent the "Failed to commit transaction (invalid or corrupted package)" error
    
    SEE: https://wiki.archlinux.org/title/Pacman#%22Failed_to_commit_transaction_(invalid_or_corrupted_package)%22_error
    SEE: https://wiki.archlinux.org/title/Pacman/Package_signing#Upgrade_system_regularly

--- functions/__pkg_update.fish
+++ functions/__pkg_update.fish
@@ -1,4 +1,9 @@
 function __pkg_update
+    # To prevent the "Failed to commit transaction (invalid or corrupted package)" error
+    # SEE: https://wiki.archlinux.org/title/Pacman#%22Failed_to_commit_transaction_(invalid_or_corrupted_package)%22_error
+    # SEE: https://wiki.archlinux.org/title/Pacman/Package_signing#Upgrade_system_regularly
+    sudo pacman -Sy --needed archlinux-keyring
+
     if test -z "$argv"
         sudo pacman -Syu
         return
